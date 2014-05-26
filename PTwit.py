5c38adcccd90bd9b5ebd47b1e17c40e72065c487
chrisdnn <cdunneg@gmail.com>
2014-05-26 01:32:21 -0500

    2nd testing Python script/streaming

--- PTwit.py
+++ PTwit.py
@@ -3,25 +3,24 @@ import json
 
 consumer_key = '6a8eiydF31fyJrJ3vJFtmpHV9'
 consumer_secret = '8tyZ34jIk2AM3WxN18JimbRC58FRyzTUSX83l7lC7Tz54rrO2H'
-access_token_key = ''
-access_token_secret = ''
-SEARCH_TERM = 'friday'
+access_token_key = '80065688-IMt9thIgmN0CM7oNK0tUGAVoC6Nkdr01XNWhl3Eu1'
+access_token_secret = 'xUjSwaK5PGAVRhFJj37U1OqJ44iISWqXAwrJOsIRaayKP'
+SEARCH_TERM = ''
 
-api = TwitterAPI(consumer_key, consumer_secret, access_token_key, access_token_secret, auth_type='oAuth2')
+#api = TwitterAPI(consumer_key, consumer_secret, access_token_key, access_token_secret, auth_type='oAuth2')
+api = TwitterAPI(consumer_key, consumer_secret, access_token_key, access_token_secret)
 
-r = api.request('geo/search', {'geocode':'29.437188, -98.500672'})
-#r = api.request('statuses/filter', {'locations':'-122.75,36.8,-121.75,37.8'})
-#r = api.request('search/tweets', {'q':SEARCH_TERM, 'geocode':'37.781157,-122.398720,1mi'})
-#r = api.request('search/tweets', {'q':SEARCH_TERM, 'geocode':'29.437188, -98.500672,25mi'})
+## Tweets from San Pedro Garza Garcia
+#r = api.request('search/tweets', {'q':SEARCH_TERM, 'geocode':'25.66173253,-100.35253104,5km','count':'450','until':'2015-05-21'})
+## with access Token
+r = api.request('statuses/filter', {'locations':'-100.598350, 25.547178, -100.036674, 25.856532'})
 
-#r = api.request('search/tweets', {'q':SEARCH_TERM, 'geocode':'19.433455, -99.146691,15665mi'})
-#r = api.request('search/tweets', {'q': SEARCH_TERM})
-#for item in r.get_iterator():
+#icount = 0
 for item in r:
-        print(json.dumps(item, indent=4, separators=(',',': ')))
-    #print('full_name: ' % item['full_name'] % ' place_type: ' % item['place_type'])
-#    j = item.json()
-#    print(j['resources']['search'])
-#    print(item.text)
+        #print(json.dumps(item, indent=4, separators=(',',': ')))
+        #print('text: ' % item['text'])
+        print(item['text'] if 'text' in item else item)
+        #icount += 1
 
-print('\nQUOTA: %s' % r.get_rest_quota())
+#print("count: %d" % icount)
+#print('\nQUOTA: %s' % r.get_rest_quota())
